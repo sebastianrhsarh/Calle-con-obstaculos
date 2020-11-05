aee5913426c17e05c5058d4a3c222d5e6b2b802b
Sebastian Riquelme <sebastianrh.sarh@gmail.com>
2020-11-05 11:47:42 -0300

    cambios

--- solve.py
+++ solve.py
@@ -4,21 +4,11 @@ deltas,board = [(-1,1),(-1,0),(-1,-1)],None
 
 
 def print_board(b):
-    """
-    Este procedimiento imprime el tablero dado por parametro b
-    """
     for l in b: 
         print(*l)
 
 
 def rebuild_path(path,tg):
-    """
-    esta funcion recibe una matriz con los padres de cada nodo --> 'path'
-    y retorna una lista con el camino para llegar hasta tg, tg es una tupla, por ejemplo --> (1,1) o (2,2)
-    que significa que se quiere conocer el camino para llegar a la posicion de la matriz (1,1) o (2,2)
-
-    Si no hay camino al tg devuelve una lista con solo el tg
-    """
     i,j = tg
     ans = [tg]
     while((i != -1 and j != -1) and path[i][j] != (i,j)):
@@ -30,11 +20,6 @@ def rebuild_path(path,tg):
 
 
 def bfs(u):
-    """
-    Esta funcion hace un bfs para encontrar el camino mas corto dese el nodo u--> el nodo u es una tupla (0,0) o (1,1)
-    desde donde inicia el bfs (coordenada de la matriz)
-
-    """
     global board
     q,found,ans,path,visited = deque(),False,list(),[[(-1,-1) for _ in range(len(board[0]))] for _ in range(len(board))],[[None for _ in range(len(board[0]))] for _ in range(len(board))]
     for i in range(len(board)):
@@ -64,7 +49,7 @@ def bfs(u):
 def solve():
     global board
     path,fit,max_fit,max_path = None,0,0,None
-    for i in range(len(board[0])):#por todas las columnas mando a hacer un bfs desde la fila 0
+    for i in range(len(board[0])):
         path,fit = bfs((len(board)-1,i))
         max_path = list(path) if fit > max_fit else max_path
         max_fit = max(fit,max_fit)
